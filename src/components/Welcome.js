62a1336faeee0f938e36ee4cfac3d0d93bbbfb38
S.SKNS <somtida.th@gmail.com>
2016-07-28 16:09:10 -0700

    webpack

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Welcome extends Component{
+  render() {
+    return(
+      <div>
+      <h1>Hello from Welcome Component</h1>
+      <p>React is cool</p>
+      </div>
+    )
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import {render} from 'react-dom'
+
+import App from './components/App';
+
+//render(
+// <App />,
+// document.getElementById('root')
+// )
+
+ReactDOM.render(
+  // <h1>Hello from HMRE</h1>,
+  <App />,
+  document.getElementById('root')
+)
