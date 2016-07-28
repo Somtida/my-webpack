23c55ea3861b0e6fc72cdb99f3ff8aca19d578dc
S.SKNS <somtida.th@gmail.com>
2016-07-28 16:40:40 -0700

    bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Welcome from './Welcome'
+import Navbar from './Navbar'
 
 export default class App extends Component {
   render(){
     return (
       <div>
+        <Navbar />
         <Welcome />
       </div>
     )
