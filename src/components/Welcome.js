b5bd781db32c252d3953396373e2311118f21432
S.SKNS <somtida.th@gmail.com>
2016-07-28 16:18:06 -0700

    component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
+// import React, { Component } from 'react'
 
-export default class Welcome extends Component{
-  render() {
-    return(
-      <div>
-      <h1>Hello from Welcome Component</h1>
+// export default class Welcome extends Component{
+//   render() {
+//     return(
+//       <div>
+//       <h1>Hello from Welcome Component</h1>
+//       <p>React is cool</p>
+//       </div>
+//     )
+//   }
+// }
+
+// export default () => {
+//   return (
+//     <div>
+//       <h1>Hello from funcitonal component!</h1>
+//       <p>React is cool</p>
+//     </div>
+//   )
+// }
+export default () => (
+    <div>
+      <h1>Hello from funcitonal component!</h1>
       <p>React is cool</p>
-      </div>
-    )
-  }
-}
+    </div>
+)
