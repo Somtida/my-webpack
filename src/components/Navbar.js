28cef1d1f945c726cb8262c28fa0d6551810aaed
S.SKNS <somtida.th@gmail.com>
2016-07-28 16:55:11 -0700

    className eslint

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Navbar extends Component {
-  render(){
+  render() {
     return (
       <div>
-      <nav class="navbar navbar-default">
-      <div class="container-fluid">
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
 
-      <div class="navbar-header">
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="#">Brand</a>
-  </div>
+            <div className="navbar-header">
+              <button
+                type="button"
+                className="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Brand</a>
+            </div>
 
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-    </ul>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+              </ul>
 
-  </div>
-</div>
-</nav>
+            </div>
+          </div>
+        </nav>
       </div>
-    )
+    );
   }
 }
