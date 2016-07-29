3efd76d1ddd7cf8e775af718137784f157ad95e7
S.SKNS <somtida.th@gmail.com>
2016-07-28 17:52:03 -0700

    submit

--- src/components/Tenants.js
+++ src/components/Tenants.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => (
-  <div>
-    <h1>Tenants</h1>
-  </div>
-);
+
+export default class Tenants extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tenants: [],
+      name: '',
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit() {
+    console.log('submit clicked', this.state);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Tenants</h1>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.setState({ name: e.target.value })}
+        />
+        <button
+          className="btn btn-success"
+          onClick={this.onSubmit}
+        >Submit</button>
+      </div>
+    );
+  }
+}
